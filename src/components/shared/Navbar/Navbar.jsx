fc7f1cf20cd4c7689229e8f0dc163d32af2857fb
Mateoskix <mateogt0903@gmail.com>
2024-10-03 17:50:58 -0500

    feat/basic project structure, constants, routing

---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//Styles
+import styles from './Navbar.module.css'
+//Icons
+
+const Navbar = () => {
+  return (
+    <header className={styles.Wrapper}>
+        <nav className={styles.Nav}>
+
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
