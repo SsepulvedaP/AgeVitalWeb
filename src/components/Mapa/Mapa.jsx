70f824c6d5e707fb47a7444bb5fac2b0623ab140
Mateoskix <mateogt0903@gmail.com>
2024-10-10 21:51:55 -0500

    feat/added map limits, custom markers and popups

--- src/components/Mapa/Mapa.jsx
+++ src/components/Mapa/Mapa.jsx
@@ -1,20 +1,52 @@
-import React from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import React, { useEffect } from "react";
+import { MapContainer, Marker, TileLayer, useMap, Popup } from "react-leaflet";
 
 //styles
 import styles from "./Mapa.module.css";
 import "leaflet/dist/leaflet.css";
+import { Icon } from "leaflet";
 
 const Mapa = () => {
   const position = [6.242391, -75.589642];
+  const bounds = [
+    [6.244529, -75.592128],
+    [6.239687, -75.586238],
+  ];
+
+  function Boundaries() {
+    const map = useMap();
+    map.setMaxBounds(bounds);
+  }
+
+  const customIcon = new Icon({
+    iconUrl: require("assets/Map_Popup_Icon.png"),
+    iconSize: [30,45]
+  });
 
   return (
     <section className={styles.Wrapper}>
-      <MapContainer center={position} zoom={20} scrollWheelZoom className={styles['leaflet-container']}>
+      <MapContainer
+        bounds={bounds}
+        zoom={20}
+        zoomControl={false}
+        scrollWheelZoom={false}
+        className={styles["leaflet-container"]}
+      >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="https://www.openstreetmap.cat" target="_blank">Breton OpenStreetMap Team</a>'
+          url="https://tile.openstreetmap.bzh/ca/{z}/{x}/{y}.png"
         />
+        <Marker icon={customIcon} position={position}>
+          <Popup className={styles["request-popup"]}>
+            <p>Sensor Humedad</p>
+            <ul>
+              <li>80 g/m3</li>
+              <li>27/09/24 16:18:20</li>
+              <li>Ubicación</li>
+            </ul>
+          </Popup>
+        </Marker>
+        <Boundaries />
       </MapContainer>
     </section>
   );
