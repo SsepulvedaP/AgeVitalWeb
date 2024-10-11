da08923594dfd8a4e03ece0ef783d584e9c835a1
Mateoskix <mateogt0903@gmail.com>
2024-10-10 19:25:28 -0500

    feat/started map implementation, added new leaflet dependencies

--- src/components/Mapa/Mapa.jsx
+++ src/components/Mapa/Mapa.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+
+//styles
+import styles from "./Mapa.module.css";
+import "leaflet/dist/leaflet.css";
 
 const Mapa = () => {
-  return (
-    <div>Mapa</div>
-  )
-}
+  const position = [6.242391, -75.589642];
 
-export default Mapa
+  return (
+    <section className={styles.Wrapper}>
+      <MapContainer center={position} zoom={20} scrollWheelZoom className={styles['leaflet-container']}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </section>
+  );
+};
+
+export default Mapa;
