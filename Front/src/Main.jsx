2b15ee37b9cb6126b2b2a0473cf4922da809df1d
Samuel <104478309+SsantaJ@users.noreply.github.com>
2024-10-28 15:56:47 -0500

    arreglos Navbar

--- Front/src/Main.jsx
+++ Front/src/Main.jsx
@@ -5,6 +5,7 @@ import Navbar from "components/shared/Navbar/Navbar";
 import Mapa from "components/Mapa/Mapa";
 import Home from "components/Home/Home";
 import Admin from "components/Admin/Admin";
+import Tresd from "components/Tresd/Tresd";
 
 function Main() {
   return (
@@ -14,6 +15,7 @@ function Main() {
         <Route path="/" exact element={<Home />} />
         <Route path="/mapa" exact element={<Mapa />} />
         <Route path="/admin" exact element={<Admin />} />
+        <Route path="/tresd" exact element={<Tresd />} />
       </Routes>
     </>
   );
--- Front/src/components/shared/Navbar/Navbar.jsx
+++ Front/src/components/shared/Navbar/Navbar.jsx
@@ -6,12 +6,10 @@ import styles from './Navbar.module.css'
 
 //Icons
 import HomeIcon from '@mui/icons-material/Home';
-import Person2Icon from '@mui/icons-material/Person2';
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
 import PlaceIcon from '@mui/icons-material/Place';
-import ViewInArIcon from '@mui/icons-material/ViewInAr';
+import ViewInArRoundedIcon from '@mui/icons-material/ViewInArRounded';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
-
 
 const Navbar = () => {
   return (
@@ -20,18 +18,19 @@ const Navbar = () => {
         <NavLink to={'/'} >
         <HomeIcon className={styles.Icon} />
         </NavLink>
-        <Person2Icon className={styles.Icon} />
         <NavLink to={'/admin'} >
         <QueryStatsIcon className={styles.Icon} />
         </NavLink>
         <NavLink to={'/mapa'}>
         <PlaceIcon className={styles.Icon} />
         </NavLink>
-        <ViewInArIcon className={styles.Icon} />
+        <NavLink to={'/Tresd'}>
+        <ViewInArRoundedIcon className={styles.Icon} />
+        </NavLink>
         <ExitToAppIcon className={styles.Exit} />
         </nav>
     </header>
   )
 }
 
-export default Navbar
+export default Navbar;
