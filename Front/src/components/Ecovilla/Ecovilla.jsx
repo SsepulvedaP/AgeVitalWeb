d2c81261a897590e9a8dea1c66ee4778f614b845
Mateoskix <mateogt0903@gmail.com>
2024-11-11 16:18:01 -0500

    added each floor images

--- Front/src/components/Ecovilla/Ecovilla.jsx
+++ Front/src/components/Ecovilla/Ecovilla.jsx
@@ -4,9 +4,15 @@ import { SwipeableDrawer, Typography } from "@mui/material";
 //Styles
 import styles from "./Ecovilla.module.css";
 import InsertChartOutlinedRoundedIcon from "@mui/icons-material/InsertChartOutlinedRounded";
+import PrimerPlanta from "assets/PrimerPlanta.png";
+import SegundaPlanta from "assets/SegundaPlanta.png";
 
 const Ecovilla = () => {
   const [pisoActual, setPisoActual] = useState("Primer Piso");
+  const ImagenPlanta =
+    pisoActual === "Primer Piso"
+      ? PrimerPlanta
+      : SegundaPlanta;
   const [open, setOpen] = React.useState(false);
 
   const toggleDrawer = (newOpen) => () => {
@@ -15,32 +21,34 @@ const Ecovilla = () => {
 
   return (
     <section className={styles.Section}>
-        <button onClick={toggleDrawer(true)} className={styles.toggleDrawer}>
-            <InsertChartOutlinedRoundedIcon/>
-        </button>
+      <button onClick={toggleDrawer(true)} className={styles.toggleDrawer}>
+        <InsertChartOutlinedRoundedIcon />
+      </button>
       <img
         src="https://www.upb.edu.co/es/imagenes/img-upbsostenibleaerea-1464235639641.jpeg"
         className={styles.Background}
       />
       <div className={styles.Wrapper}>
         <h1>{pisoActual}</h1>
-      <SwipeableDrawer
-        anchor="right"
-        open={open}
-        onClose={toggleDrawer(false)}
-        onOpen={toggleDrawer(true)}
-        disableSwipeToOpen={false}
-        ModalProps={{
-          keepMounted: true,
-        }}
-      >
-        <button onClick={() => setPisoActual("Primer Piso")}>
+        <img src={ImagenPlanta} alt="" className={styles.Map} />
+        <SwipeableDrawer
+          anchor="right"
+          open={open}
+          sx={{ padding: "2rem" }}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+          disableSwipeToOpen={false}
+          ModalProps={{
+            keepMounted: true,
+          }}
+        >
+          <button onClick={() => setPisoActual("Primer Piso")}>
             <Typography>Primer Piso</Typography>
-        </button>
-        <button onClick={() => setPisoActual("Segundo Piso")}>
+          </button>
+          <button onClick={() => setPisoActual("Segundo Piso")}>
             <Typography>Segundo Piso</Typography>
-        </button>
-      </SwipeableDrawer>
+          </button>
+        </SwipeableDrawer>
       </div>
     </section>
   );
